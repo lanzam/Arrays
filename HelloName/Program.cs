2b1e1841a15a8c109d55d16e13c08372bc314f8c
Mariell <Mariell@DESKTOP-V0EECRJ>
2019-01-15 17:59:51 -0500

    Version 2 HelloName

--- HelloName/Program.cs
+++ HelloName/Program.cs
@@ -6,7 +6,9 @@ namespace HelloName
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "Mariel Lanza";
+            Console.WriteLine( name );
+            Console.ReadKey(true);
         }
     }
 }
